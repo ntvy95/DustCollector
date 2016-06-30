f64a8573d31187a6e70b530ca8d5955fcd0095cf
ntvy95 <ntvy95@gmail.com>
2016-06-30 09:46:19 +0700

    ...

--- AgentOrientedProgramming/bin/Debug/HutBuiUpdate.pl
+++ AgentOrientedProgramming/bin/Debug/HutBuiUpdate.pl
@@ -12,13 +12,13 @@ facing(0) :- done(start).
 discover(0,0) :- done(start), assert(discovered(0,0)).
 timePassed(0) :- done(start).
 
-facing(Temp):- wasfacing(A), Temp is A+90, done(turn90).
-facing(A):-wasfacing(A),done(forward).
-in(Temp1, Temp2):- wasin(X,Y), wasfacing(A), Temp1 is X + round(cos(pi*A/180)), Temp2 is Y + round(sin(pi*A/180)), done(forward).
-weight(Temp1,Temp2,inf) :- in(X,Y), (A is 0;A is 90;A is 180;A is 270), Temp1 is X + round(cos(pi*A/180)), Temp2 is Y + round(sin(pi*A/180)), obstacle(Temp1,Temp2,static), (done(forward);done(start)).
-weight(X,Y,Temp) :- wasin(X,Y), not(useful(X,Y)), (wasweight(X,Y,W);not(wasweight(X,Y,_)),W is 1), Temp is W*10, done(forward).
-weight(X,Y,Temp):- wasin(X,Y), useful(X,Y), (wasweight(X,Y,W);not(wasweight(X,Y,_)),W is 0), Temp is W+1, done(forward).
+facing(Temp):- done(turn90), wasfacing(A), Temp is A+90.
+facing(A):-done(forward),wasfacing(A).
+in(Temp1, Temp2):- done(forward), wasin(X,Y), wasfacing(A), Temp1 is X + round(cos(pi*A/180)), Temp2 is Y + round(sin(pi*A/180)).
+weight(Temp1,Temp2,inf) :- (done(forward);done(start)), in(X,Y), (A is 0;A is 90;A is 180;A is 270), Temp1 is X + round(cos(pi*A/180)), Temp2 is Y + round(sin(pi*A/180)), obstacle(Temp1,Temp2,static).
+weight(X,Y,Temp) :- done(forward), wasin(X,Y), not(useful(X,Y)), (wasweight(X,Y,W);not(wasweight(X,Y,_)),W is 1), Temp is W*10.
+weight(X,Y,Temp):- done(forward), wasin(X,Y), useful(X,Y), (wasweight(X,Y,W);not(wasweight(X,Y,_)),W is 0), Temp is W+1.
 discover(Temp1, Temp2):- (done(forward);done(start)),in(X,Y), facing(M), (A is M;A is M+90;A is M+180;A is M+270), Temp1 is X + round(cos(pi*A/180)), Temp2 is Y + round(sin(pi*A/180)), not(obstacle(Temp1, Temp2, static)), not(discovered(Temp1, Temp2)).
 leftmoves(TempK):- (done(forward);done(start)), in(X,Y), facing(M), (A is M;A is M+90;A is M+180;A is M+270), Temp1 is X + round(cos(pi*A/180)), Temp2 is Y + round(sin(pi*A/180)), not(obstacle(Temp1, Temp2, static)), not(discovered(Temp1, Temp2)), retract(wasleftmoves(K)), TempK is K + 1, assert(wasleftmoves(TempK)).
-leftmoves(TempK):- in(X,Y), not(wasweight(X,Y,_)), retract(wasleftmoves(K)), TempK is K - 1, assert(wasleftmoves(TempK)), done(forward).
+leftmoves(TempK):- done(forward), in(X,Y), not(wasweight(X,Y,_)), retract(wasleftmoves(K)), TempK is K - 1, assert(wasleftmoves(TempK)).
 useful(X,Y) :- (A is 0;A is 90;A is 180;A is 270), Temp1 is X + round(cos(pi*A/180)), Temp2 is Y + round(sin(pi*A/180)), not(wasweight(Temp1,Temp2,_)).
